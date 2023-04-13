173e069e8a077fefcd466783e5515e5c3dd85a70
Ethan Willers <willers.enw@gmail.com>
2023-04-12 19:29:32 -0700

    added pages directory with Home.js to render

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 
 const client = new ApolloClient({
   uri: "/graphql",
