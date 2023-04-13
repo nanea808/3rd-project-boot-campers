c12a80bd44ef9c360d6abbd0bbeeeb106503ce21
Ethan Willers <willers.enw@gmail.com>
2023-04-13 15:37:03 -0700

    wrote custom request logic for jwt headers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import {
   ApolloClient,
-  InMemoryCache,
   ApolloProvider,
-  // createHttpLink,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+  concat,
 } from "@apollo/client";
-// import { setContext } from '@apollo/client/link/context';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //components
@@ -16,24 +18,25 @@ import Home from "./pages/Home";
 import User from "./pages/User";
 import LoginPage from "./pages/LoginPage";
 
+const httpLink = new HttpLink({ uri: '/graphql' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || null,
+    }
+  }));
+
+  return forward(operation);
+})
+
 //apollo
 const client = new ApolloClient({
-  uri: "/graphql",
   cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
 });
-
-// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
 
 function App() {
   return (
