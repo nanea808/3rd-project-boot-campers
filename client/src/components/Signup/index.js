ec906f4d02644c54f7f014ce76252154ce8f10ea
Sarah <sarah.k.hopper@gmail.com>
2023-04-12 19:21:07 -0700

    begin refactor of validation using yup schema

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,59 +1,78 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
 
 //input validation
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (!/^[a-zA-Z]+{50,}$/i.test(values.firstName)) { 
-        errors.firstName = 'Must be 50 letters or less. No special characters, symbols, or numbers.'; 
-    }
+// not needed if using yup validation schema
+// const validate = values => {
+//     const errors = {};
+//     if (!values.firstName) {
+//         errors.firstName = 'Required';
+//     } else if (!/^[a-zA-Z]+{50,}$/i.test(values.firstName)) { 
+//         errors.firstName = 'Must be 50 letters or less. No special characters, symbols, or numbers.'; 
+//     }
 
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (!/^[a-zA-Z-]+{50,}$/i.test(values.lastName)) {
-        errors.lastName = 'Must be 50 letters or less. No special characters, symbols, or numbers.'
-    }
+//     if (!values.lastName) {
+//         errors.lastName = 'Required';
+//     } else if (!/^[a-zA-Z-]+{50,}$/i.test(values.lastName)) {
+//         errors.lastName = 'Must be 50 letters or less. No special characters, symbols, or numbers.'
+//     }
 
-    if (!values.email) {
-        errors.email = 'Required';
+//     if (!values.email) {
+//         errors.email = 'Required';
     
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
+//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//         errors.email = 'Invalid email address';
+//     }
     
-        return errors;
-};
+//         return errors;
+// };
 
 
 //signup form
 
 const SignupForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: ''
-        },
+    // const formik = useFormik({
+    //     initialValues: {
+    //         firstName: '',
+    //         lastName: '',
+    //         email: ''
+    //     },
         
         // onSubmit: values => {
         //     alert(JSON.stringify(values, null, 2));
-        validate,
-            onSubmit: values => {
-                alert(JSON.stringify(values, null, 2));
-        },
-    });
-
+    //     validate,
+    //         onSubmit: values => {
+    //             alert(JSON.stringify(values, null, 2));
+    //     },
+    // });
     return (
+        <Formik
+            initialValues={{ firstName: '', lastName: '', email: ''}}
+            onSubmit={(values, { setsubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setsubmitting(false);
+                }, 400);
+            }}
+
+        >
+
+
+        </Formik>
+    )
+
+    // return (
         <form onSubmit={formik.handleSubmit}>
-            <label htmlFor='firstname'>First Name</label>
+            <label htmlFor='firstName'>First Name</label>
             <input
                 id='firstName'
                 name='firstName'
                 type='text'
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
+                {...formik.getFieldProps('firstName')}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.firstName}
             />
 
             {/* {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null} */}
@@ -67,8 +86,10 @@ const SignupForm = () => {
                 id='lastName'
                 name='lastName'
                 type='text'
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
+                {...formik.getFieldProps('lastName')}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.lastName}
             />
             {/* {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null} */}
             {formik.touched.lastName && formik.errors.lastName ? (
@@ -80,8 +101,10 @@ const SignupForm = () => {
                 id='email'
                 name='email'
                 type='email'
-                onChange={formik.handleChange}
-                value={formik.values.email}
+                {...formik.getFieldProps('email')}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.email}
             />
             {/* {formik.errors.email ? <div>{formik.errors.email}</div> : null} */}
             {formik.touched.email && formik.errors.email ? (
