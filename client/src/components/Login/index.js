dd042af179f2f41ce83aa94710775811bd16eb43
Sarah <sarah.k.hopper@gmail.com>
2023-04-12 21:28:31 -0700

    refactor login componenet

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,43 +1,98 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+//yup validation schema
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .required('Email required')
+        .email('Invalid email'),
+    password: Yup.string()
+        .required('Password required')
+        .min(8, 'Password must be at least 8 characters'),
+});
 
 const Login = () => {
-    <div>
-        <h1>Login</h1>
-        <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
-                const errors = {};
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address. Please check your spelling and try again.';
-         }
-         return errors;
-       }}
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={schema}
+            //         onSubmit={(values, { setSubmitting }) => {
+            //             setTimeout(() => {
+            //                 alert(JSON.stringify(values, null, 2));
+            //                 setSubmitting(false);
+            //             }, 400)
+            //         }}
+                    onSubmit={(values) => {
+                        alert(JSON.stringify(values));
+                    }}
+            >
+                {/* {({ isSubmitting }) => { */}
 
-       onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 400)
-       }}
-       >
-        {({ isSubmitting }) => {
-            <Form>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" component="div" />
-                <button type="submit" disabled={isSubmitting}>
-                    Log In
-                </button>
-            </Form>
-        }}
-       </Formik>
-    </div>
+                {({ 
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                }) => {
+                    <div className='login'>
+                        <Form noValidate onSubmit={handleSubmit}>
+                            <div>
+                                <h1>Login</h1>
+                            </div>
+                            <div>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                    placeholder='Enter email'
+                                    id='email'
+                                    // className='' //add formatting class here
+                                />
+
+                                <p className='error'>
+                                    {errors.email && touched.email && errors.email}
+                                </p>
+
+                                <input
+                                    type='password'
+                                    name='password'
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                    placeholder='Enter password'
+                                    // className='' //add formatting here
+                                />
+
+                                <p className='error'>
+                                    {errors.password && touched.password && errors.password}
+                                </p>
+
+                            </div>
+                            {/* <Field type="email" name="email" />
+                            <ErrorMessage name="email" component="div" />
+                            <Field type="password" name="password" />
+                            <ErrorMessage name="password" component="div" /> */}
+
+                            <button type="submit" disabled={isSubmitting}>
+                                Log In
+                            </button>
+
+                        </Form>
+
+                    </div>
+                }}
+                </Formik>
+            </div>
+        </>
+    );
 }
 
 export default Login;
