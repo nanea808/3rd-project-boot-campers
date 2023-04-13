177004fc2a03e362c123a5a5177c6ec23c0eacd9
Ethan Willers <willers.enw@gmail.com>
2023-04-12 20:37:19 -0700

    navbar test fixed

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
+
+function LoginPage() {
+    const [isLoggingIn, setLoggingIn] = useState(true);
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default LoginPage;
