ed4765244bc44dea0e8334d5805ca28bec2c1e01
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-04-13 19:33:40 -0700

    login page styles

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,85 +1,178 @@
+// import React from 'react';
+// import { ReactDOM } from 'react-dom';
+// // import { Formik, Form, Field, ErrorMessage } from 'formik'; //Field, ErrorMessage being rejected by react :/
+// import { Formik, Form } from 'formik';
+// import * as Yup from 'yup';
+
+// import Auth from '../../auth';
+
+// //yup validation schema
+// const schema = Yup.object().shape({
+//     email: Yup.string()
+//         .required('Email required')
+//         .email('Invalid email'),
+//     password: Yup.string()
+//         .required('Password required')
+//         .min(8, 'Password must be at least 8 characters'),
+// });
+
+// //TODO: 
+// // onSubmit, implement desired result (redirect to user profile with additional features available)
+// // add reset button
+// // add styling
+// // add security measures to protect/obscure password?
+// const LoginForm = () => {
+//     return (
+//         <>
+//           {/* Wrapping form inside formik tag and passing our schema to validationSchema prop */}
+//             <Formik
+//                 validationSchema={schema}
+//                 initialValues={{ email: "", password: "" }}
+//                 onSubmit={(values) => {
+//                     // comment this out and replace with desired functionality (e.g., redirect to user profile page logged-in view)
+//                 //   alert(JSON.stringify(values));
+//                 console.log("User email and password passed validation. Values: " + JSON.stringify(values));
+//                 }}
+//             >
+//                 {({
+//                 values,
+//                 errors,
+//                 touched,
+//                 handleChange,
+//                 handleBlur,
+//                 handleSubmit,
+//                 }) => (
+//                 <div className="login">
+//                     <div className="form">
+//                     <form noValidate onSubmit={handleSubmit}>
+//                         <h1>Login</h1>
+//                         <input
+//                         type="email"
+//                         name="email"
+//                         onChange={handleChange}
+//                         onBlur={handleBlur}
+//                         value={values.email}
+//                         placeholder="you@email.com"
+//                         //   className="form-control inp_text"
+//                         id="email"
+//                         />
+//                         {/* show error if validation failed */}
+//                         <p className="error">
+//                         {errors.email && touched.email && errors.email}
+//                         </p>
+//                         <input
+//                         type="password"
+//                         name="password"
+//                         onChange={handleChange}
+//                         onBlur={handleBlur}
+//                         value={values.password}
+//                         placeholder="Enter password"
+//                         //   className="form-control"
+//                         />
+//                         {/* show error if validation failed */}
+//                         <p className="error">
+//                         {errors.password && touched.password && errors.password}
+//                         </p>
+//                         <button type="submit">Login</button>
+//                     </form>
+//                     </div>
+//                 </div>
+//                 )}
+//             </Formik>
+//             </>
+//         );    
+// }
+
+// export default LoginForm;
+
 import React from 'react';
-import { ReactDOM } from 'react-dom';
-// import { Formik, Form, Field, ErrorMessage } from 'formik'; //Field, ErrorMessage being rejected by react :/
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
-//yup validation schema
+import Auth from '../../auth';
+
+// yup validation schema
 const schema = Yup.object().shape({
-    email: Yup.string()
-        .required('Email required')
-        .email('Invalid email'),
-    password: Yup.string()
-        .required('Password required')
-        .min(8, 'Password must be at least 8 characters'),
+  email: Yup.string()
+    .required('Email required')
+    .email('Invalid email'),
+  password: Yup.string()
+    .required('Password required')
+    .min(8, 'Password must be at least 8 characters'),
 });
 
-//TODO: 
-// onSubmit, implement desired result (redirect to user profile with additional features available)
-// add reset button
-// add styling
-// add security measures to protect/obscure password?
 const LoginForm = () => {
-    return (
-        <>
-          {/* Wrapping form inside formik tag and passing our schema to validationSchema prop */}
-            <Formik
-                validationSchema={schema}
-                initialValues={{ email: "", password: "" }}
-                onSubmit={(values) => {
-                    // comment this out and replace with desired functionality (e.g., redirect to user profile page logged-in view)
-                //   alert(JSON.stringify(values));
-                console.log("User email and password passed validation. Values: " + JSON.stringify(values));
-                }}
-            >
-                {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                }) => (
-                <div className="login">
-                    <div className="form">
-                    <form noValidate onSubmit={handleSubmit}>
-                        <h1>Login</h1>
-                        <input
-                        type="email"
-                        name="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        placeholder="you@email.com"
-                        //   className="form-control inp_text"
-                        id="email"
-                        />
-                        {/* show error if validation failed */}
-                        <p className="error">
-                        {errors.email && touched.email && errors.email}
-                        </p>
-                        <input
-                        type="password"
-                        name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                        placeholder="Enter password"
-                        //   className="form-control"
-                        />
-                        {/* show error if validation failed */}
-                        <p className="error">
-                        {errors.password && touched.password && errors.password}
-                        </p>
-                        <button type="submit">Login</button>
-                    </form>
-                    </div>
+  return (
+    <div className="login-container">
+      {/* Wrapping form inside formik tag and passing our schema to validationSchema prop */}
+      <Formik
+        validationSchema={schema}
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          // comment this out and replace with desired functionality (e.g., redirect to user profile page logged-in view)
+          console.log('User email and password passed validation. Values: ' + JSON.stringify(values));
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <div className="login">
+            <div className="form">
+              <h1 className="heading">Login</h1>
+              <Form noValidate onSubmit={handleSubmit}>
+                <div className="input-group">
+                  <label htmlFor="email" className="label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    placeholder="you@email.com"
+                    className="input"
+                    id="email"
+                  />
+                  {/* show error if validation failed */}
+                  {errors.email && touched.email && (
+                    <p className="error">{errors.email}</p>
+                  )}
                 </div>
-                )}
-            </Formik>
-            </>
-        );    
-}
+                <div className="input-group">
+                  <label htmlFor="password" className="label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    placeholder="Enter password"
+                    className="input"
+                    id="password"
+                  />
+                  {/* show error if validation failed */}
+                  {errors.password && touched.password && (
+                    <p className="error">{errors.password}</p>
+                  )}
+                </div>
+                <button type="submit" className="button">
+                  Login
+                </button>
+              </Form>
+            </div>
+          </div>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default LoginForm;
-
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/Login';
+import Footer from '../components/Footer';
 // import SignupForm from '../components/Signup';
 
 function LoginPage() {
     const [isLoggingIn, setLoggingIn] = useState(true);
 
     return (
+        <main>
         <div>
             <p>this text</p>
             {isLoggingIn ? <LoginForm /> : <h1>signup</h1>}
         </div>
+        <Footer />
+        </main>
     );
 }
 
