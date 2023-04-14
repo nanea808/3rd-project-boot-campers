2d4adef3f50cf5662857aadf0c3ea8c40f06e00e
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-04-13 19:00:55 -0700

    user seed sans picture

---
+++ server/seeders/seed.js
@@ -0,0 +1,15 @@
+// users have the following values:
+// picture
+// username
+// email
+// password
+// description
+// skills list (array)
+
+// tasks have the following values:
+// id
+// name
+// description
+// current funding
+// owning user
+// assigned user
