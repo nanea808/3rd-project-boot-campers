b4cc63087d55143e576daa4fcf941b631efcd501
Ethan Willers <willers.enw@gmail.com>
2023-04-16 19:09:19 -0700

    global context working.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,18 +8,18 @@ import SavedTasks from "../components/SavedTasks";
 import { QUERY_UNCLAIMED_TASKS } from "../api/queries";
 import { QUERY_WATCHED_TASKS } from "../api/queries";
 
-// import { Navigate, useParams } from 'react-router-dom';
+import { useAccountContext } from '../context/GlobalState';
 
 function Home() {
   const unclaimedQuery= useQuery(QUERY_UNCLAIMED_TASKS);
   const unclaimedTasks = unclaimedQuery.data?.unclaimedTasks || {};
   // const {userId: userParam} = useParams();
-  const userId = "643b9515196f12fe7796060e"
-
+  // const userId = "643b74774cd8e64cc511791b";
+  const [state, dispatch] = useAccountContext();
 
   // temporarily using UserOnes ID 
   const watchedQuery = useQuery(QUERY_WATCHED_TASKS, {
-    variables: {userId: userId}
+    variables: {userId: state.userID}
   });
   const watchedTasks = watchedQuery.data?.user[0].watchedTasks || {};
 
